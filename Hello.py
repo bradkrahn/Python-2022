25c8d15be7c03aad960f29293507ff4f39747adf
Your Namebradkrahn <bradk@krahn.com>
2022-07-08 21:23:22 -0700

    Initial commit

---
+++ Hello.py
@@ -0,0 +1,25 @@
+import os
+os.system('clear')
+
+###########################
+# DATA TYPES
+# Strings
+# Numbers
+# Lists
+# Tuples
+# Dictionaries
+# Boolean
+###########################
+
+first_name = 'Brad'
+Age = 28
+names = ("John","Mary","Joe")
+
+fav_pizza = {
+	"John": "pepperoni",
+	"Mary": "cheese",
+	"Joe": "pineapple"
+}
+
+print (Age)
+print (names) 
